53b704a167a4eea9c22ca801be547aa64bb18bdb
suminhan123 <suminh324@gmail.com>
2023-05-18 12:21:26 +0900

    #7 feat: Setting React Query
    -redux 세팅 pull 받아온 후 react-query 세팅 완료
    -redux오류 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
+import React from 'react';
+
 import '~/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
+import {
+  QueryClient,
+  QueryClientProvider,
+  Hydrate,
+} from '@tanstack/react-query';
 
 import wrapper from '~/store';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
   const { store, props } = wrapper.useWrappedStore(pageProps);
 
   return (
-    <Provider store={store}>
-      <Component {...props} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedProps}>
+        <Provider store={store}>
+          <Component {...props} />
+        </Provider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
