e7425b581a95a4764df3d9a64571d6cbfc673c67
suminhan123 <suminh324@gmail.com>
2023-05-19 13:06:24 +0900

    #9 feat: react-query, mui, styled-component 다시 setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,25 @@
-import '~/styles/globals.css';
+import React from 'react';
+
 import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import GlobalStyle from '~/styles/global-styles';
 
 import wrapper from '~/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
+  // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
+  const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <Provider store={store}>
-      <Component {...props} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <GlobalStyle />
+        <Component {...props} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
