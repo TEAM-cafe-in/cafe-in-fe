f8e60a662acf73679caf3b4a92297a16fc1f72d9
suminhan123 <suminh324@gmail.com>
2023-05-21 22:54:16 +0900

    #11 feat: Add react-query default option

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app';
 
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyle from '~/styles/global-styles';
 
@@ -11,16 +12,33 @@ import wrapper from '~/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
-  // 이렇게 해야 서로 다른 사용자와 요청 사이에 데이터가 공유되지 않는다.
-  const [queryClient] = React.useState(() => new QueryClient());
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      // onError : handleError => 공통 에러 처러 hook 만들기
+      queries: {
+        // staleTime을 변경하여 리엑트 쿼리에개 캐시된 데이터를 얼마나 자주 최신화 시켜줘야 하는지 알려줄 수 있다.(중복 호출 방지)
+        // 쿼리 마다 다르게 시간을 지정 가능
+        staleTime: 1000 * 20,
+        retry: 0,
+        // API가 실패하면 설정한 값만큼 재시도 하는 옵션
+        suspense: true,
+        useErrorBoundary: true,
+      },
+      // 예기치 못한 에러 케이스가 생길 수 있기에 queries와 mutations에 true 값으로 설정
+      mutations: {
+        useErrorBoundary: true,
+      },
+    },
+  });
 
   return (
-    // react query로는 서버데이터를 관리하므로 redux보다 상위에 위치해 두었습니다
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
         <GlobalStyle />
         <Component {...props} />
-      </Provider>
-    </QueryClientProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
   );
 }
