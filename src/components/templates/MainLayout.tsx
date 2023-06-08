3c9c3fe1778e84cf8735fa9db22c7f885d4a6e8d
decodingdog <gowl4622@gmail.com>
2023-06-08 14:00:54 +0900

    #16 style: Modify main layout

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -5,30 +5,38 @@
 
 import { ReactNode, useCallback, useState } from 'react';
 
-import { Box, useMediaQuery } from '@mui/material';
+import { Box, Toolbar, useMediaQuery } from '@mui/material';
 
 import Logo from '~/static/images/logo.png';
-import Profile_01 from '~/static/images/Profile_01.png';
 
 import { DrawerItem } from '~/types/drawer';
-import Header from '../organism/header';
+import Image from 'next/image';
+import AppBar from '../organism/appBar';
 import Drawer from '../organism/drawer';
+import Profile from '../atom/profile';
+import BottomSheet from '../organism/bottomSheet';
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const drawerItems: DrawerItem[] = [
-  { name: 'logo', src: Logo, text: '' },
-  { name: 'mypage', src: Profile_01, text: '마이' },
+  {
+    name: 'logo',
+    text: '',
+    children: <Image src={Logo} alt="로고 이미지" width={30} height={30} />,
+  },
+  { name: 'mypage', text: '마이', children: <Profile size="sm" /> },
 ];
+
+const query = '(min-width:0px) and (max-width:600px)';
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   /**
-   * Media Query Hook
+   * Media Query Hook (SSR에서 동작)
    * 너비가 [0-360] 픽셀이면 true를 반환
    */
-  const isMobile = useMediaQuery('(min-width:0px) and (max-width:360px)');
+  const isMobile = useMediaQuery(query, { noSsr: false });
 
   // 선택중인 메뉴
   const [selectedItem, setSelectedItem] = useState('logo');
@@ -40,9 +48,10 @@ const MainLayout = ({ children }: MainLayoutProps) => {
 
   return (
     <Box style={{ display: 'flex' }}>
-      {/* Header 영역 */}
-      {isMobile && <Header />}
-      {/* Side Menu 영역 */}
+      {/* 모바일 AppBar 영역 */}
+      {isMobile && <AppBar />}
+
+      {/* PC 용 Side Menu 영역 */}
       {!isMobile && (
         <Drawer
           data={drawerItems}
@@ -50,11 +59,16 @@ const MainLayout = ({ children }: MainLayoutProps) => {
           handleSelectedItem={handleSelectedItem}
         />
       )}
-      {/* Side Menu에 해당하는 Drawer 영역 */}
 
-      {/* 현재 라우팅된 페이지 */}
       <Box component="main" sx={{ flexGrow: 1, p: 3, backgroundColor: 'gray' }}>
+        {/* Mobile 용 AppBar 영역만큼 차지해줌 */}
+        {isMobile && <Toolbar />}
+
+        {/* 현재 라우팅된 페이지 */}
         {children}
+
+        {/* Mobile 전용 BottomSheet 영역  */}
+        {isMobile && <BottomSheet />}
       </Box>
     </Box>
   );
--- src/types/drawer.ts
+++ src/types/drawer.ts
@@ -2,11 +2,10 @@
  * @createdBy 김해지
  * @description Drawer 관련 타입 정의 모음
  */
-
-import { StaticImageData } from 'next/image';
+import { ReactNode } from 'react';
 
 export interface DrawerItem {
   name: string;
-  src: StaticImageData;
   text: string;
+  children: ReactNode;
 }
