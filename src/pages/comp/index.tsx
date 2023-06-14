36c5d744c2f7d1e65497607a070017cfa7721fd4
decodingdog <gowl4622@gmail.com>
2023-06-15 00:16:59 +0900

    #19 feat: Add Toast Component

--- src/pages/comp/index.tsx
+++ src/pages/comp/index.tsx
@@ -7,6 +7,7 @@ import { Box } from '@mui/material';
 import ButtonPreview from '~/components/organism/preview/ButtonPreview';
 import ModalPreview from '~/components/organism/preview/ModalPreview';
 import PopupPreview from '~/components/organism/preview/PopupPreview';
+import ToastPreview from '~/components/organism/preview/ToastPreview';
 
 const Comp = () => {
   return (
@@ -18,6 +19,8 @@ const Comp = () => {
       <PopupPreview />
       <h4>Modal Component</h4>
       <ModalPreview />
+      <h4>Toast Message Component</h4>
+      <ToastPreview />
     </Box>
   );
 };
