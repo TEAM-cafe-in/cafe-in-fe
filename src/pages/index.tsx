912907d383dc1ed48b433bc286646b35da38f98c
suminhan123 <suminh324@gmail.com>
2023-07-11 15:45:45 +0900

    #30 feat: Change access token, user Redux to SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,22 @@
-// import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import { GetServerSidePropsContext } from 'next';
 
 import { Box } from '@mui/material';
-// import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 
-// import { setToken } from '~/store/reducers/authSlice';
-// import { setUserData } from '~/store/reducers/userSlice';
+import { setToken, useAccessTokenSelector } from '~/store/reducers/authSlice';
+import { setUserData, useAccessUserSelector } from '~/store/reducers/userSlice';
 // Google Maps 페이지
 
 import GoogleMapComponent from '~/components/organism/googleMap';
-// import { setCookie } from '~/helpers/cookie';
-// import wrapper from '~/store';
-import { User } from '~/types/user';
+import wrapper from '~/store';
 
-// import { getAccessToken, getUserData } from './api/user';
+import { getAccessToken, getUserData } from './api/user';
 
-interface HomeProps {
-  accessToken: string;
-  userData: User;
-}
-
-const Home = (props: HomeProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { accessToken, userData } = props;
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //  dispatch(setToken({ access_token: accessToken }));
-  //  dispatch(
-  //    setUserData({
-  //      isLoggedIn: true,
-  //      email: userData.email,
-  //      memberId: userData.memberId,
-  //      memberName: userData.memberName,
-  //      profile: userData.profile,
-  //      role: userData.role,
-  //    })
-  //  );
-  //  localStorage.setItem('accessToken', accessToken);
-  //  setCookie('accessToken', accessToken, {});
-  // }, [accessToken, dispatch, userData]);
+const Home = () => {
+  const token = useAccessTokenSelector();
+  console.log('리덕스 엑세스 토큰', token);
+  const user = useAccessUserSelector();
+  console.log('리덕스 사용자 정보', user);
 
   return (
     <Box>
@@ -51,24 +27,36 @@ const Home = (props: HomeProps) => {
 
 export default Home;
 
-// export const getServerSideProps: GetServerSideProps =
-//  wrapper.getServerSideProps(
-//    () => async (context: GetServerSidePropsContext) => {
-//      const { req } = context;
-//      // 서버에서 쿠키값 접근하기
-//      const cookies = new Cookies(req.headers.cookie);
-//      const refreshToken = cookies.get('refreshToken');
-//      // 쿠키에 있는 refresh 토큰값으로 access 토큰 재발급
-//      // const res = await getAccessToken(refreshToken);
-//      // console.log(res);
-//      // access 토큰으로 사용자 정보 받아오기
-//      // const user = await getUserData(res.accessToken, 'USER');
-//      // store.dispatch(cafeInfo);
-//      // return {
-//      //  props: {
-//      //    accessToken: res.accessToken,
-//      //    userData: user,
-//      //  },
-//      // };
-//    }
-//  );
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async (context: GetServerSidePropsContext) => {
+    const { req } = context;
+
+    // 서버에서 쿠키값 접근하기
+    const cookies = new Cookies(req.headers.cookie);
+    const refreshToken = cookies.get('refreshToken');
+
+    // 쿠키에 있는 refresh 토큰값으로 access 토큰 재발급
+    const res = await getAccessToken(refreshToken);
+    const { accessToken } = res;
+
+    // access 토큰으로 사용자 정보 받아오기
+    const user = await getUserData(res.accessToken, 'USER');
+
+    // store.dispatch를 사용하여 액션을 호출(토큰과 사용자 정보 저장)
+    store.dispatch(setToken({ access_token: accessToken }));
+    store.dispatch(
+      setUserData({
+        isLoggedIn: true,
+        email: user.email,
+        memberId: user.memberId,
+        memberName: user.memberName,
+        profile: user.profile,
+        role: user.role,
+      })
+    );
+
+    return {
+      props: {},
+    };
+  }
+);
